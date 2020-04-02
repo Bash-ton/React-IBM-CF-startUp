fbddb0b2fe6052bcee8adc97dc5a47d00fe5a8b7
Bash-ton <sebbepaz@gmail.com>
2020-04-02 23:53:17 +0200

    added /build

---
+++ build/precache-manifest.d689471b4be1ae8611a96c1e6f65845b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a26fcef770ef51a7d8a56db943a56f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbe169a7a3a772903b9b",
+    "url": "/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "75f86fbb17ed33daf1e3",
+    "url": "/static/js/2.f8857bef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8857bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe169a7a3a772903b9b",
+    "url": "/static/js/main.bcf48b47.chunk.js"
+  },
+  {
+    "revision": "a6c1338449dee9649233",
+    "url": "/static/js/runtime-main.93fc8f18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
